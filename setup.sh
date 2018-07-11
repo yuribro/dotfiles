ad1b5e6d0c0ba2dcdb18089ef1f5853494918771
yuribro <yuri@guardicore.com>
2018-07-11 13:10:47 +0300

    More base16 changes

--- setup.sh
+++ setup.sh
@@ -12,5 +12,18 @@ then
 	ln -s ${DIR}/bin ${HOME}/bin
 fi
 
-[ ! -e ${HOME}/.Xresources ] && ln -s ${HOME}/etc/.Xresources ${HOME}/.Xresources
-[ ! -e ${HOME}/.emacs.d ] && ln -s ${HOME}/etc/emacs ${HOME}/.emacs.d
+install_file() {
+	[ ! -e ${HOME}/$1 ] && ln -s ${HOME}/etc/$1 ${HOME}/$1
+	
+}
+
+install_file .Xresources
+install_file .tmux.conf
+install_file .emacs.d
+
+if [ ! -d ${HOME}/.config/base16-shell/ ]
+then
+	git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+	source /home/yuri/repos/dotfiles/etc/base16.sh
+fi
+
